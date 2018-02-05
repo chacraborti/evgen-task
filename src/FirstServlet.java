a486daf1e8ef5ab7862dbe5f73cbd839d2ec3b1c
Ilona Sidarovich <ilona_sidarovich@epam.com>
2018-02-05 18:10:18 +0300

    first commit

---
+++ src/FirstServlet.java
@@ -0,0 +1,33 @@
+
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Logger;
+
+public class FirstServlet extends HttpServlet {
+
+    Logger logger = Logger.getLogger("logger");
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1>Hi there</h1>");
+        out.println("</body>");
+        out.println("</html>");
+
+        logger.warning("doGet was called");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
