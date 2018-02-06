9f5607c805b0e99d079dd9a5a4598ca4c5113d58
Ilona Sidarovich <ilona_sidarovich@epam.com>
2018-02-06 18:11:00 +0300

    v2

--- src/FirstServlet.java
+++ src/FirstServlet.java
@@ -1,21 +1,22 @@
-
-
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 public class FirstServlet extends HttpServlet {
 
     Logger logger = Logger.getLogger("logger");
 
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
         PrintWriter out = resp.getWriter();
         out.println("<html>");
         out.println("<body>");
@@ -23,11 +24,50 @@ public class FirstServlet extends HttpServlet {
         out.println("</body>");
         out.println("</html>");
 
-        logger.warning("doGet was called");
+        writeToLog(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
+        writeToLog(req, resp);
     }
+
+
+
+    private void writeToLog(HttpServletRequest req, HttpServletResponse resp) {
+        getRequestHeadersInfo(req).entrySet().stream().forEach(e -> logger.warning("request header from doGet: " + e.toString()));
+        resp.getHeaderNames().stream().forEach(e -> logger.warning("request header from doGet: " + e.toString()));
+
+        getRequestBodyInfo(req).entrySet().stream().forEach(e -> logger.warning("request body from doGet: " + e.toString()));
+        resp.
+    }
+    private Map<String, String> getRequestHeadersInfo(HttpServletRequest request) {
+
+        Map<String, String> map = new HashMap<>();
+
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            String value = request.getHeader(key);
+            map.put(key, value);
+        }
+
+        return map;
+    }
+
+    private Map<String, String> getRequestBodyInfo(HttpServletRequest request) {
+
+        Map<String, String> map = new HashMap<>();
+
+        Enumeration names = request.getParameterNames();
+        while (names.hasMoreElements()) {
+            String key = (String) names.nextElement();
+            String value = request.getParameter(key);
+            map.put(key, value);
+        }
+
+        return map;
+    }
+
 }
---
+++ src/filter/FilterForLogger.java
@@ -0,0 +1,31 @@
+package filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Logger;
+
+@WebFilter(urlPatterns = { "/*" })
+public class FilterForLogger implements Filter {
+
+    Logger logger = Logger.getLogger("logger");
+
+    public void init(FilterConfig fConfig) throws ServletException {
+    }
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+        final PrintWriter writer = new PrintWriter(response.getWriter());
+        chain.doFilter(request, new HttpServletResponseWrapper((HttpServletResponse) response) {
+            @Override public PrintWriter getWriter() {
+                return writer;
+            }
+        });
+        logger.log(writer.getCopy());
+    }
+    public void destroy() {
+        code = null;
+    }
+}
